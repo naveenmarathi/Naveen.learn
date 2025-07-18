b3b19f523dce2cc95a6a421e20429b760057fc81
Ubuntu <ubuntu@ip-10-0-0-116.ap-south-1.compute.internal>
2025-07-18 08:20:26 +0000

    changes made in multi.shfile

---
+++ add.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Read two numbers from the user
+echo "Enter first number:"
+read num1
+
+echo "Enter second number:"
+read num2
+
+# Add the numbers
+sum=$((num1 + num2))
+
+# Print the result
+echo "The sum is: $sum"
+echo" No changes made"
+echo" small changes made"
---
+++ multi.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Read two numbers from the user
+echo "Enter first number:"
+read num1
+
+echo "Enter second number:"
+read num2
+
+# Multiply the numbers
+product=$((num1 * num2))
+
+# Print the result
+echo "The product is: $product"
+
